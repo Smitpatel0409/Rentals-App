ad912d64cee4f8283c93ea673fdb8f128d32396a
Smit Patel <psmit9077@gmail.com>
2024-09-11 22:42:39 +0530

    Checking the Dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout() {
   }, [loaded])
 
   console.log('loaded', loaded)
+  console.log('hel')
 
   if (!loaded) {
     return null
@@ -35,7 +36,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
