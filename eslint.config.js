c709ed544aee9a22f3edbcf6ab726d21f074aa8b
Smit Patel <psmit9077@gmail.com>
2024-09-12 07:38:09 +0530

    updating the Dependencies

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+// eslint.config.js
+
+module.export = [
+    {
+        files: ['**/*.{js,jsx,ts,tsx}'],
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true
+                }
+            }
+        },
+        rules: {
+            'no-console': ['error', { allow: ['warn', 'error'] }],
+            'prettier/prettier': 'error'
+        },
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        }
+    }
+];
